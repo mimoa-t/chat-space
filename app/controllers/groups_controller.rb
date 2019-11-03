1c2322a769e9f391068fc896e67613f2b1c84508
mimoa-t <mimoa.toki@gmail.com>
2019-11-03 12:55:25 +0900

    CREATE format

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,6 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
   def index
-    render "messages/index"
   end
 
   def new
@@ -18,7 +17,6 @@ class GroupsController < ApplicationController
     end
   end
 
-    # groups_pathを編集する
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: "グループを編集しました"
@@ -29,7 +27,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, user_ids: [] )
   end
 
   def set_group
